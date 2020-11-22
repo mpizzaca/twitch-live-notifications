5d1d9a23e743840a6d526e6d15cbd0e51dac3709
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2020-11-21 19:29:41 -0500

    inc

---
+++ index.js
@@ -0,0 +1,34 @@
+// npm modules
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const session = require('express-session');
+const MongoClient = require('mongodb').MongoClient;
+const MongoStore = require('connect-mongo')(session);
+
+// create the server
+const app = express();
+
+// add & configure middleware
+app.use(session({
+    genid: (req) => {
+        console.log('Inside session middleware')
+        console.log(req.sessionID)
+        return uuidv4()
+    },
+    secret: 'asdjaiwd',
+    resave: false,
+    saveUninitialized: true
+}))
+
+
+// create homepage route at '/'
+app.get('/', (req,res) => {
+    console.log('Inside the homepage callback function')
+    console.log(req.sessionID)
+    res.send('homepage');
+})
+
+// start the server
+app.listen(3005, () => {
+    console.log('Listening on localhost:3005');
+})
