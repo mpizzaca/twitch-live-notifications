de5a06af8db07fc7831cdd379c4794e5469ebd13
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2020-11-24 21:51:26 -0500

    user management w/ passport/session

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 // npm modules
 const express = require('express')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 const mongoose = require('mongoose')
+const passport = require('passport')
 const LogRequest = require('./logRequest')
+const User = require('./models/Users')
+const bcrypt = require('bcrypt')
 
 const secrets = require('./secrets')
-const dbUrl = 'mongodb://localhost:27017/mydb'
-const dbName = 'twitch-live-notifications-db'
+const dbUrl = process.env.MONGODB_URL || secrets.MONGODB_URL
+//const dbName = 'twitch-live-notifications-db'
+
+require('./config/passport')(passport)
+
+// configure mongoose
+mongoose.connect(dbUrl, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
 
 // create the server
 const app = express();
@@ -18,40 +30,92 @@ app.use(session({
     secret: secrets.expressSessionSecret,
     resave: false,
     saveUninitialized: true,
+    store: new MongoStore ({ mongooseConnection: mongoose.connection })
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 
-// configure mongoose
-mongoose.connect(dbUrl, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-})
 
-/*********************/
+
+//*******************//
 //     ENDPOINTS     //
-/*********************/
+//*******************//
 // create homepage route at '/'
 app.get('/', (req,res) => {
     res.send('homepage');
 })
 
 app.get('/login', (req, res) => {
-    console.log('GET /login');
     console.log('req.sessionID: ' + req.sessionID);
     res.send('login page');
 })
 
-app.post('/login', (req,res) => {
-    
-    res.send('default login response')
-})
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) { return next(err) }
+        if (!user) { 
+            // authentication failed
+            console.log('login for ' + req.body.username + ' failed: ' + JSON.stringify(info))
+            return res.redirect('/login') 
+        }
 
-    
+        req.login(user, err => {
+            if (err) throw err
+            
+        })
+
+        return res.send('login successful')
+    })(req, res, next)
+})
 
 app.post('/register', (req,res) => {
 
-    res.send('default registration response')
+    const { username, password } = req.body
+    let errors = []
+
+    if (!username || !password) { errors.push({ msg: 'All fields are required'}) }
+    if (password.length < 7) { errors.push({ msg: 'Password must be at least 7 characters'}) }
+
+    if (errors.length > 0) {
+        return res.send(errors)
+    } else {
+        User.findOne({ username: username}).exec((err, user) => {
+            if(user) {
+                errors.push({ msg: 'Username already in use'})
+                return res.send(errors + user)
+            } else {
+                // new user
+                const newUser = new User({
+                    username,
+                    password
+                })
+
+                // hash password
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err
+                        newUser.password = hash
+                        newUser.save()
+                        .then(value => {
+                            console.log('value: ' + value)
+                            res.redirect('/users/login')
+                        })
+                        .catch( value => console.log(value) )
+                        
+                    })
+                })
+            }
+        })
+    }
 })
 
+app.get('/user/:username', (req, res) => {
+    if (req.isAuthenticated() && req.user.username == req.params.username) {
+        res.send('this is your private profile')
+    } else {
+        res.status(401).send('you are not authorized to view this page')
+    }
+})
 
 
 // start the server
