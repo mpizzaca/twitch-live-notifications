8cb4457f5bea8d82f9c6baa99c93173c080a0931
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-14 21:54:12 -0400

    code cleanup

--- public/worker.js
+++ public/worker.js
@@ -1,19 +1,17 @@
 console.log('Service Worker loaded!')
 
+// When push event is received, display notification to the user (you!?)
 self.addEventListener('push', event => {
-    const mData = event.data.json();
-    console.log('Got push', mData);
-    const { title, icon, actions, data } = mData;
-    
-    event.waitUntil(
-        self.registration.showNotification(title, {
-            icon,
-            actions,
-            data
-        })
-    )
-})
+    //console.log('Got push', event.data.json());
+    const { title, icon, actions, data } = event.data.json();
+    const contents = { icon, actions, data };
+    const sentNotification = self.registration.showNotification(title, contents);
+    event.waitUntil(sentNotification);
+});
 
+
+
+// When notification is clicked, go to provided url (ie. the Twitch stream)
 self.addEventListener('notificationclick', function(event) {
     clients.openWindow(event.notification.data.url)
-})
+});
