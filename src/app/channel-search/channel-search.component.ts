e60285ef573236f45449ce4529bfda8bd2e60756
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-26 21:47:55 -0400

    filter subscribed channels from search results, re-filter when subscriptions update to remove subscribed-to channels from search results

--- src/app/channel-search/channel-search.component.ts
+++ src/app/channel-search/channel-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
 import {
   debounceTime,
   distinctUntilChanged,
@@ -15,7 +15,7 @@ import { ChannelService } from '../channel.service';
   styleUrls: ['./channel-search.component.scss'],
 })
 export class ChannelSearchComponent implements OnInit {
-  channels$!: Observable<Channel[]>;
+  channels: Channel[] = [];
   private searchTerms = new Subject<string>();
 
   constructor(private channelService: ChannelService) {}
@@ -25,19 +25,34 @@ export class ChannelSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.channels$ = this.searchTerms.pipe(
-      debounceTime(300),
-      distinctUntilChanged(),
-      switchMap((term: string) => this.channelService.searchChannels(term)),
-      map((channels) => {
-        // filter already subscribed-to channels from search results
-        return channels.filter(
-          (channel) =>
-            this.channelService.channels.value.filter(
-              (existingChannel) => existingChannel.name === channel.name
-            ).length === 0
-        );
-      })
+    this.searchTerms
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((term: string) => this.channelService.searchChannels(term)),
+        map((channels) => {
+          // filter already subscribed-to channels from search results
+          return channels.filter((channel) =>
+            this.filterSubscribedChannels(channel)
+          );
+        })
+      )
+      .subscribe((searchResults) => (this.channels = searchResults));
+
+    // Setup observer for subscribed channels
+    // when this changes, we want to re-filter results
+    this.channelService.channels.subscribe(() => {
+      this.channels = this.channels.filter((channel) =>
+        this.filterSubscribedChannels(channel)
+      );
+    });
+  }
+
+  private filterSubscribedChannels(channel: Channel): boolean {
+    return (
+      this.channelService.channels.value.filter(
+        (exChannel) => exChannel.name === channel.name
+      ).length === 0
     );
   }
 }
