e833c08c5c308f6b4f96ff5e8da5617d1f07e6d1
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-28 00:18:13 -0400

    add Authorized() helper to check if user is authorized

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { Channel } from './channel';
 
 @Injectable({
@@ -8,6 +9,7 @@ import { Channel } from './channel';
 })
 export class ApiService {
   private readonly ENDPOINTS = {
+    root: 'http://localhost:3005',
     channels: 'http://localhost:3005/channels',
     subscription: 'http://localhost:3005/subscription',
   };
@@ -20,10 +22,33 @@ export class ApiService {
     }),
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.Authorized();
+  }
+
+  Authorized(): Observable<boolean> {
+    // No token header -> not authorized
+    if (!this.httpOptions.headers.get('Authorization')) {
+      return of(false);
+    } else {
+      // We have a token - check with server if it's for a valid user
+      return this.http.get(this.ENDPOINTS.root, this.httpOptions).pipe(
+        // if the HTTP request is successful will return true, otherwise false
+        map(() => true),
+        catchError(() => of(false))
+      );
+    }
+  }
 
   getChannels(): Observable<Channel[]> {
-    return this.http.get<Channel[]>(this.ENDPOINTS.channels, this.httpOptions);
+    return this.http
+      .get<Channel[]>(this.ENDPOINTS.channels, this.httpOptions)
+      .pipe(
+        catchError((err) => {
+          console.log('getChannels error:', err);
+          return of([]);
+        })
+      );
   }
 
   getChannelsByName(term: string): Observable<Channel[]> {
