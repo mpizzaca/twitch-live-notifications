273e0271aaf23c6ae2b78ac1f04deb82859d9ce2
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-27 23:11:34 -0400

    linting

--- frontend/src/custom-service-worker.js
+++ frontend/src/custom-service-worker.js
@@ -1,8 +1,6 @@
 importScripts("./ngsw-worker.js");
 
 (function () {
-  "use strict";
-
   self.addEventListener("notificationclick", (event) => {
     if (clients.openWindow && event.notification.data.url) {
       event.waitUntil(clients.openWindow(event.notification.data.url));
