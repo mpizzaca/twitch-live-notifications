3a9e9d4926c202915c89f3abb6e1c38f6274ea13
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-31 17:35:45 -0400

    update notification buttons to a slider/toggle

--- frontend/src/app/notification/notification.component.ts
+++ frontend/src/app/notification/notification.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NotificationService } from '../services/notification.service';
 
 @Component({
@@ -7,10 +7,24 @@ import { NotificationService } from '../services/notification.service';
   styleUrls: ['./notification.component.scss'],
 })
 export class NotificationComponent implements OnInit {
+  @Input() isChecked!: boolean;
+
   constructor(private notificationService: NotificationService) {}
 
   ngOnInit(): void {
     this.notificationService.observeSubscription();
+    this.isChecked = JSON.parse(localStorage.getItem('user')!)
+      .webpushSubscription
+      ? true
+      : false;
+  }
+
+  notificationsToggle(): void {
+    if (this.isChecked) {
+      this.subscribeToPushNotifications();
+    } else {
+      this.unsubscribeFromPushNotifications();
+    }
   }
 
   subscribeToPushNotifications(): void {
