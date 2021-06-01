e74c094c937d77cad73c47475b001ab82a17248e
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-05-31 20:38:34 -0400

    redesign channel-list-item

--- frontend/src/app/channel-list-item/channel-list-item.component.ts
+++ frontend/src/app/channel-list-item/channel-list-item.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Channel } from '../channel';
 import { ChannelService } from '../services/channel.service';
+import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+import { ChannelSearchComponent } from '../channel-search/channel-search.component';
+
 @Component({
   selector: 'app-channel-list-item',
   templateUrl: './channel-list-item.component.html',
@@ -8,13 +11,19 @@ import { ChannelService } from '../services/channel.service';
 })
 export class ChannelListItemComponent implements OnInit {
   @Input() channel!: Channel;
+  faPlusCircle = faPlusCircle;
+  faMinusCircle = faMinusCircle;
 
-  constructor(private channelService: ChannelService) {}
+  constructor(
+    private channelService: ChannelService,
+    private channelSearch: ChannelSearchComponent
+  ) {}
 
   ngOnInit(): void {}
 
   subscribe(channel: Channel): void {
     this.channelService.subscribe(channel);
+    this.channelSearch.clearSearch();
   }
 
   unsubscribe(channel: Channel): void {
